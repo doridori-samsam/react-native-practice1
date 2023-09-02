0dc1825d50c3ef893b3295b9885210ef6bdea3bf
Sam <wtawonlife@gmail.com>
2023-09-03 01:10:36 +0900

    first commit

--- App.js
+++ App.js
@@ -4,17 +4,23 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hellooooo, this is Sam!!! </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//StyleSheet.create를 안쓰고 일반 객체로 써도 된다...
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: "orangered",
+    textDecorationLine: "underline",
   },
 });
